120f632524a3db45cf374403a80b72534470d615
lenonmerlo <lenontm@gmail.com>
2025-10-20 18:02:16 -0300

    feat(user): e-mail + unicidade; hash com BCrypt; @PrePersist p/ timestamps; validações e checagens no service

--- src/main/java/com/fiap/fintech/repository/UserRepository.java
+++ src/main/java/com/fiap/fintech/repository/UserRepository.java
@@ -3,9 +3,7 @@ package com.fiap.fintech.repository;
 import com.fiap.fintech.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
